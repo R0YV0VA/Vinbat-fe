f70cd66f6a9821249332a6d1a83797ec0bfc797d
kruzhambus <maks.mrug.ru@gmail.com>
2023-04-30 15:07:33 +0300

    refactor: work logic with api

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,35 @@
+import axios from 'axios'
+import debug from 'debug'
+import _isEmpty from 'lodash/isEmpty'
 
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+})
+
+debug('API: %s', process.env.REACT_APP_API_URL)
+
+api.interceptors.request.use(
+  (config) => {
+    const token = ''
+    // get token func
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+export const get = () =>
+  api
+    .get('url')
+    .then((response) => response.data)
+    .catch((error) => {
+      debug('%s', error)
+      throw _isEmpty(error.response.data?.message)
+        ? error.response.data
+        : error.response.data.message
+    })
